2c987e46840d0a0019364c981d0d78cb2fa37b67
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2021-11-05 11:12:52 +0200

    impruve edit, add, btns

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
 function onPageLoaded () {
   const newNote = document.querySelector('.newNoteArea');
+  const headingNote = document.querySelector('.headingInput');
   const notes = document.querySelector('.notes');
   let noteId = 0;
   let editedNoteId;
 
   function clear () {
+    document.querySelector('.headingInput').value = '';
     document.querySelector('.newNoteArea').value = '';
   }
 
-  function createBtn(mainElem, name, cb) {
+  function createBtn(mainElem, name, blacklight, cb) {
     const elem = document.createElement('div');
     elem.classList.add(name);
+    elem.classList.add(blacklight);
     const srcImg = `img/${name}.png`;
     const iconElem = new Image();
     iconElem.src = srcImg;
@@ -33,21 +36,29 @@ function onPageLoaded () {
     note.classList.add('note');
     note.setAttribute('id', createId());
 
+    const headingText = document.createElement('p');
+    headingText.classList.add('headingNote');
     const notesText = document.createElement('p');
     notesText.classList.add('notesText');
     
     const newText = newNote.value;
-    notesText.append(newText);
-    notes.appendChild(note);
-    note.appendChild(notesText)
+    const newHeading = headingNote.value;
 
-    createBtn(note, 'del', (mainElem) => {
+    notes.appendChild(note);
+    note.appendChild(headingText);
+    note.appendChild(notesText);
+    notesText.append(newText);
+    headingText.append(newHeading);
+
+    createBtn(note, 'del', 'blacklighRed', (mainElem) => {
       mainElem.remove();
     });
     
-    createBtn(note, 'edit', (mainElem) => {
+    createBtn(note, 'edit', 'blacklighYelow', (mainElem) => {
       const editNote = mainElem.querySelector('.notesText');
+      const editHeading = mainElem.querySelector('.headingNote');
       newNote.value = editNote.textContent;
+      headingNote.value = editHeading.textContent;
 
       const btnEdit = document.getElementById('edit');
       btnEdit.style.display = 'block';
@@ -55,32 +66,39 @@ function onPageLoaded () {
       btnAdd.style.display = 'none';
       
       editNote.textContent = '';
+      editHeading.textContent = '';
 
       editedNoteId = mainElem.getAttribute('id');
     });
 
-    createBtn(note, 'ready', () => {
-      if (notesText.style.backgroundColor !== 'gray') {
+    createBtn(note, 'ready', 'blacklighGreen', () => {
+      if (notesText.style.backgroundColor !== 'gray' 
+          && headingText.style.backgroundColor !== 'gray') {
         notesText.style.backgroundColor = 'gray';
         notesText.style.textDecoration = 'line-through';
-
+        headingText.style.backgroundColor = 'gray';
+        headingText.style.textDecoration = 'line-through';
+        
         return
       }
 
       notesText.style.backgroundColor = 'aquamarine';
       notesText.style.textDecoration = 'none';
+      headingText.style.backgroundColor = 'rgb(60, 161, 228)';
+      headingText.style.textDecoration = 'none';
     });
 
     clear();
 
-    newNote.focus();
+    headingNote.focus();
   }
 
-  // TODO: need to improve
   function editNote() {
     const elem = document.getElementById(editedNoteId);
     const editNote = elem.querySelector('.notesText');
     editNote.textContent = newNote.value;
+    const currHeadingNote = elem.querySelector('.headingNote');
+    currHeadingNote.textContent = headingNote.value;
     
     const btnEdit = document.getElementById('edit');
     btnEdit.style.display = 'none';
@@ -89,11 +107,17 @@ function onPageLoaded () {
 
     clear();
 
-    newNote.focus();
+    headingNote.focus();
   }
 
   const add = document.getElementById('add');
   add.onclick = createNote;
+
+  newNote.addEventListener('keyup', function(event) {
+    if (event.key == 'Enter' && event.shiftKey) {
+    createNote();
+    }
+  });
 
   const edit = document.getElementById('edit');
   edit.onclick = editNote;
