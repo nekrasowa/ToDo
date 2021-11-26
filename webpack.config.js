48b02ce4624c90058646dae37ea3bddd4a161f75
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2021-11-26 16:13:57 +0200

    customization

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './start/index.js',
@@ -6,4 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+    ],
+}
 };
