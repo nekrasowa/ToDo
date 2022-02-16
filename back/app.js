34c391ba2dcd13b3fd044622bd35c965103fcbfc
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-02-16 13:55:35 +0200

    add POST for new Note

--- back/app.js
+++ back/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 4000
 
@@ -6,18 +7,38 @@ const oldNotesArr = [
   {
     heading: 'Первая', 
     text: 'и текст', 
-    ready: false 
+    ready: false,
+    id: 'note-0' 
   },
   {
     heading: 'Вторая', 
     text: 'И ее текст', 
-    ready: false
+    ready: false,
+    id: 'note-1'
   }
 ]
 
+app.use(cors())
+app.use(express.json())
+
 app.get('/', function(req, res) {
-  res.set('Access-Control-Allow-Origin', '*')
   res.json(oldNotesArr)
+})
+
+app.post('/', function(req, res) {
+  try {
+    console.log('[POST-/]:', req.body)
+  
+    oldNotesArr.push(req.body)
+
+    console.log(`Note is added!`)
+    console.log(oldNotesArr)
+
+    res.json({ isOk: true })
+  } catch (err) {
+    res.status(500)
+    res.json({ isOk: false })
+  }
 })
 
 app.listen(port, function() {
