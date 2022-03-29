c24b82492a826970cb20c05b1fc5936b654d8e68
Lucy Nekrasova <l.w.nekrasova@gmail.com>
2022-03-29 17:02:22 +0300

    add indexes

--- workingBack/dbKonoha.js
+++ workingBack/dbKonoha.js
@@ -1,62 +1,73 @@
-import {
-    addShinobi
-  } from './shinobiArray.js'
-  
-  const Naruto = {
-    'name': { 'first': 'Naruto', 'klan': 'Udsumaki' },
-    'rank': 'genin',
-    'power': 50,
-    'element': 'wind',
-    'type of attack': 'close combat',
-    'feature': 'jinchuriki'
-  }
-  
-  const Sakura = {
-    'name': { 'first': 'Sakura', 'klan': 'Haruno' },
-    'rank': 'chunin',
-    'power': 40,
-    'element': '',
-    'type of attack': ['close combat', 'medical technique'],
-    'feature': 'medic'
-  }
-  
-  const Sasuke = {
-    'name': { 'first': 'Sasuke', 'klan': 'Uchiha' },
-    'rank': 'genin',
-    'power': 55,
-    'element': ['fire', 'lightning'],
-    'type of attack': ['distance fighting', 'medical technique'],
-    'feature': ['sharingan', 'sword']
-  }
-  
-  const Kakashi = {
-    'name': { 'first': 'Kakashi', 'klan': 'Hataki' },
-    'rank': 'dsenin',
-    'power': 60,
-    'element': ['fire', 'lightning', 'earth', 'woter'],
-    'type of attack': ['close combat', 'medical technique'],
-    'feature': 'sharingan'
-  }
+const {
+  addShinobi,
+  getShinobi,
+  findShinobi,
+  addShinobiTown
+} = require('./shinobiArray.js')
+
+const Naruto = {
+  'name': { 'first': 'Naruto', 'klan': 'Udsumaki' },
+  'rank': 'genin',
+  'power': 50,
+  'element': 'wind',
+  'type of attack': 'close combat',
+  'feature': 'jinchuriki'
+}
+const Sakura = {
+  'name': { 'first': 'Sakura', 'klan': 'Haruno' },
+  'rank': 'chunin',
+  'power': 40,
+  'element': '',
+  'type of attack': ['close combat', 'medical technique'],
+  'feature': 'medic'
+}
+const Sasuke = {
+  'name': { 'first': 'Sasuke', 'klan': 'Uchiha' },
+  'rank': 'genin',
+  'power': 55,
+  'element': ['fire', 'lightning'],
+  'type of attack': ['distance fighting', 'medical technique'],
+  'feature': ['sharingan', 'sword']
+}
+const Kakashi = {
+  'name': { 'first': 'Kakashi', 'klan': 'Hataki' },
+  'rank': 'dsenin',
+  'power': 60,
+  'element': ['fire', 'lightning', 'earth', 'woter'],
+  'type of attack': ['close combat', 'medical technique'],
+  'feature': 'sharingan'
+}
 
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = "mongodb://localhost:27017"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 const dbName = 'Konoha'
-const db = client.db(dbName)
-const col = db.collection('shinobi')
-
 
 async function run() {
   try {
       await client.connect()
+      const db = await client.db(dbName)
       console.log('Connected correctly to server')
-      addShinobi(Naruto, col)
+      const col = db.collection('shinobi')
+      // await col.createIndex({element: 1, power: 1})
+      // const indexes = await col.indexes()
+      // console.log('[indexes]:', indexes)
+
+      await col.find({element: 'earth', power: 30})
+        .toArray(function(err, results){
+        console.log(results)
+        console.log(results.length)
+      })
+      // console.log('end');
+
   } catch (err) {
       console.log(err.stack)
   }
-  finally {
-      // await client.close()
-  }
+  // finally {
+  //   console.log('end');
+  // }
 }
 run().catch(console.dir)
 
+// and or and
+// если первый енд не срабатывает будет ли работать дальше другие
--- workingBack/shinobiArray.js
+++ workingBack/shinobiArray.js
@@ -1,40 +1,4 @@
-const Naruto = {
-  'name': { 'first': 'Naruto', 'klan': 'Udsumaki' },
-  'rank': 'genin',
-  'power': 50,
-  'element': 'wind',
-  'type of attack': 'close combat',
-  'feature': 'jinchuriki'
-}
-
-const Sakura = {
-  'name': { 'first': 'Sakura', 'klan': 'Haruno' },
-  'rank': 'chunin',
-  'power': 40,
-  'element': '',
-  'type of attack': ['close combat', 'medical technique'],
-  'feature': 'medic'
-}
-
-const Sasuke = {
-  'name': { 'first': 'Sasuke', 'klan': 'Uchiha' },
-  'rank': 'genin',
-  'power': 55,
-  'element': ['fire', 'lightning'],
-  'type of attack': ['distance fighting', 'medical technique'],
-  'feature': ['sharingan', 'sword']
-}
-
-const Kakashi = {
-  'name': { 'first': 'Kakashi', 'klan': 'Hataki' },
-  'rank': 'dsenin',
-  'power': 60,
-  'element': ['fire', 'lightning', 'earth', 'woter'],
-  'type of attack': ['close combat', 'medical technique'],
-  'feature': 'sharingan'
-}
-
-export async function addShinobi(name, col) {
+async function addShinobi(name, col) {
   try {
     await col.insertOne(name)
     await col.findOne({ 'first': `${name}`});
@@ -43,4 +7,65 @@ export async function addShinobi(name, col) {
     console.log(err.stack)
   }
 }
+async function getShinobi(col) {
+  try {
+    await col.find().toArray(function(err, results){
+                 
+    console.log(results)
+    })
+  } catch (err) {
+    console.log(err.stack)
+  }
+}
+async function findShinobi(col) {
+  try {
+    await col.find({rank: 'genin'}).toArray(function(err, results){
+                 
+    console.log(results)
+    })
+  } catch (err) {
+    console.log(err.stack)
+  }
+}
+
+function arrayRandElement(arr) {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
+
+function getRandomPower(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const element = ['fire', 'lightning', 'earth', 'woter', 'wind']
+const type = ['close combat', 'medical technique', 'distance fighting']
+const feature = ['jinchuriki', 'medic', 'sharingan', 'kikajchu', 'bjakugan', 'pinegan', 'hidsutsu', 'tajdjitsu']
+const rank = ['genin', 'chuunin', 'dzenin', 'ANBU', 'hokage']
+
+async function addShinobiTown(col) {
+  try {
+    let idName 
+
+    for (idName = 26354; idName < 1300000; idName++) {
+      const id = {
+        'name': idName,
+        'rank': arrayRandElement(rank),
+        'power': getRandomPower(10, 150),
+        'element': [arrayRandElement(element), arrayRandElement(element), arrayRandElement(element)],
+        'type of attack': [arrayRandElement(type), arrayRandElement(type), arrayRandElement(type)],
+        'feature': [arrayRandElement(feature), arrayRandElement(feature), arrayRandElement(feature)]
+      }
+      await col.insertOne(id)
+    }
+  } catch (err) {
+    console.log(err.stack)
+  }
+}
+module.exports = {
+  findShinobi,
+  addShinobi,
+  getShinobi,
+  addShinobiTown
+}
+
 
